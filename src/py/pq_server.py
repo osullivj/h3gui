a729351691b52324c4536707f65f28a5733523c5
John O'Sullivan <osullivj@gmail.com>
2025-02-25 16:51:19 +0000

    fix for standalone op w/breadboard

--- src/py/pq_server.py
+++ src/py/pq_server.py
@@ -12,30 +12,23 @@ import nd_consts
 import nd_web
 import nd_utils
 
-logr = nd_utils.init_logging(__name__)
+NDAPP='pq_server'
+
+logr = nd_utils.init_logging(NDAPP)
 
 EXTRA_HANDLERS = [
     (r'/api/parquet/(.*)', nd_web.ParquetHandler, dict(path=os.path.join(nd_consts.ND_ROOT_DIR, 'dat')))
 ]
 
-class PQApp(nd_web.NDAPIApp):
-    def __init__(self):
-        super().__init__(extra_handlers=EXTRA_HANDLERS)
+define("port", default=8891, help="run on the given port", type=int)
 
-
-define("port", default=443, help="run on the given port", type=int)
-
+service = nd_utils.Service(NDAPP, {}, {}, False)
 
 async def main():
     parse_command_line()
-    cert_path = os.path.normpath(os.path.join(nd_consts.ND_ROOT_DIR, 'cfg', 'h3'))
-    app = PQApp()
-    https_server = tornado.httpserver.HTTPServer(app, ssl_options={
-        "certfile": os.path.join(cert_path, "ssl_cert.pem"),
-        "keyfile": os.path.join(cert_path, "ssl_key.pem"),
-    })
-    https_server.listen(options.port)
-    logr.info(f'{__file__} port:{options.port} cert_path:{cert_path}')
+    app = nd_web.NDApp(service, EXTRA_HANDLERS)
+    app.listen(options.port)
+    logr.info(f'{NDAPP} port:{options.port}')
     await asyncio.Event().wait()
 
 
